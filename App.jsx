2c7bde5eb6cb30338fbf57bc462c8d0812f4595d
memecoins-dweb <chipembashamiso@gmail.com>
2025-06-01 18:17:28 +0200

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TokenLauncher from './components/TokenLauncher';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <TokenLauncher />
+    </div>
+  );
+}
+
+export default App;
