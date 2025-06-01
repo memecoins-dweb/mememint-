cfd2ff961730bd132c607bdc736643814a730696
memecoins-dweb <chipembashamiso@gmail.com>
2025-06-01 18:39:12 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -10,3 +10,17 @@ function App() {
 }
 
 export default App;
+import React from 'react';
+import TokenLauncher from './components/TokenLauncher';
+import DepositWithdraw from './components/DepositWithdraw';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <TokenLauncher />
+      <DepositWithdraw />
+    </div>
+  );
+}
+
+export default App;
